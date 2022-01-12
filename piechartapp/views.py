969e54c6987723e00e9d96deb617e3933f187bf1
mac <mac@macui-MacBookPro-65.local>
2022-01-12 12:03:23 +0900

    [feat] piechart 더미 변경

--- piechartapp/views.py
+++ piechartapp/views.py
@@ -8,6 +8,14 @@ from django.utils.decorators import method_decorator
 
 import datetime
 
+def make_dummy_interval_info(interval_id: int, start_time: str, end_time: str, label: str):
+    interval = dict()
+    interval["id"] = interval_id
+    interval["startTime"] = start_time
+    interval["endTime"] = end_time
+    interval["labels"] = [label]
+    return interval
+
 
 def make_dummy_piechart_info():
     content = dict()
@@ -16,22 +24,29 @@ def make_dummy_piechart_info():
     content["data"] = dict()
     content["data"]["id"] = 132
 
-    # make interval1 info
-    interval1 = dict()
-    interval1["id"] = 2
-    interval1["startTime"] = "2022-01-04 10:45:10"
-    interval1["endTime"] = "2022-01-04 17:45:10"
-    interval1["labels"] = ["회사"]
-    # make interval2 info
-    interval2 = dict()
-    interval2["id"] = 3
-    interval2["startTime"] = "2022-01-04 17:45:10"
-    interval2["endTime"] = "2022-01-04 22:45:10"
-    interval2["labels"] = ["?"]
-
     content["data"]["info"] = list()
-    content["data"]["info"].append(interval1)
-    content["data"]["info"].append(interval2)
+
+    content["data"]["info"].append(
+        make_dummy_interval_info(1, "2022-01-12 00:00:00", "2022-01-12 09:00:00", "A장소")
+    )
+    content["data"]["info"].append(
+        make_dummy_interval_info(2, "2022-01-12 09:00:00", "2022-01-12 09:52:00", "이동(차)")
+    )
+    content["data"]["info"].append(
+        make_dummy_interval_info(3, "2022-01-12 09:52:00", "2022-01-12 11:58:00", "B장소")
+    )
+    content["data"]["info"].append(
+        make_dummy_interval_info(4, "2022-01-12 11:58:00", "2022-01-12 12:08:00", "이동(도보)")
+    )
+    content["data"]["info"].append(
+        make_dummy_interval_info(5, "2022-01-12 12:08:00", "2022-01-12 12:43:00", "C장소")
+    )
+    content["data"]["info"].append(
+        make_dummy_interval_info(6, "2022-01-12 12:43:00", "2022-01-12 12:52:00", "이동(도보)")
+    )
+    content["data"]["info"].append(
+        make_dummy_interval_info(7, "2022-01-12 12:52:00", "2022-01-12 13:34:00", "B장소")
+    )
 
     return content
 

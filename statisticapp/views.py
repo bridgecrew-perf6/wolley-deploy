8685b1a3a7d256b99b7e36045534762848528f02
mac <mac@macui-MacBookPro-65.local>
2022-02-16 09:10:08 +0900

    [feat] week 정보 불러오기 추가

--- statisticapp/views.py
+++ statisticapp/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -5,7 +7,10 @@ from rest_framework import status
 
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+
+from accountapp.models import AppUser
 from myapi.utils import make_response_content
+from statisticapp.models import WeekInfo, WeekCategoryInfo
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -13,6 +18,14 @@ class StatRequestView(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request):
+        request_user = request.headers['user']
+        request_date = request.headers['date']
+        year,month,_ = map(int,request_date.split('-'))
+        week = datetime(year, month, 1).isocalendar().week
+
+        user = AppUser.objects.get(user__username=request_user)
+        week_info = WeekInfo.objects.get(user=user, year=year, month_order=month, week_order=week)
+        week_category_info = WeekCategoryInfo.objects.filter(week_info=week_info, name="이동")
 
         data = {
             "labels": [

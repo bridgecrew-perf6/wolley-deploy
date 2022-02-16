9c34ba80fcf9a70585dc130ece305749bebedfce
Alphatechnic <alphatechnicist@gmail.com>
2022-02-16 09:06:16 +0900

    [refactor] import myapi.setting.__init__ 으로 변경 & noti 관련 파일들 notification 폴더에 정리

--- myapi/settings/__init__.py
+++ myapi/settings/__init__.py
@@ -2,15 +2,9 @@
 from .deploy import *  # deploy mode
 
 """
-local mode <-> deploy mode 스위치 때 수정해야할 곳
+local mode <-> deploy mode 스위치 때 수정
 
-1.
 myapi/settings/__init__.py 
 # from .local import *  # local mode
 from .deploy import *  # deploy mode
-
-2.
-dailypathapp/utils.py
-# from myapi.settings.local import *
-from myapi.settings.deploy import *
 """

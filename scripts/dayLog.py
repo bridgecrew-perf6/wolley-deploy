3cf311359dd543c8933996626026ca9858b3db6f
Alphatechnic <alphatechnicist@gmail.com>
2022-02-16 15:51:31 +0900

    [refactor] uuid와 날짜로 data를 받아볼 수 있는 로직 작성

--- scripts/dayLog.py
+++ scripts/dayLog.py
@@ -11,9 +11,24 @@ django.setup()
 def run(username, date):
     date = datetime.datetime.strptime(date, '%Y-%m-%d')
     gpslogs = GPSLog.objects.filter(
-                                daily_path__user__user__username__icontains=username,
-                                timestamp__year=date.year,
-                                timestamp__month=date.month,
-                                timestamp__day=date.day,
-                                )
-    print(gpslogs)
+        daily_path__user__user__username__icontains=username,
+        timestamp__year=date.year,
+        timestamp__month=date.month,
+        timestamp__day=date.day,
+    )
+
+    data_to_print = dict()
+    if len(gpslogs) != 0:
+        gpslog_obj = gpslogs[0]
+        username = gpslog_obj.daily_path.user.user.username
+        data_to_print["user"] = username
+
+    data_to_print["timeSequence"] = list()
+    for gpslog in gpslogs:
+        timestamp = dict()
+        timestamp["time"] = gpslog.timestamp.strftime('%Y-%m-%d %H:%M:%S')
+        timestamp["coordinates"] = dict()
+        timestamp["coordinates"]["latitude"] = gpslog.latitude
+        timestamp["coordinates"]["longitude"] = gpslog.longitude
+        data_to_print["timeSequence"].append(timestamp)
+    print(data_to_print)

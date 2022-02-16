b5dbe0349690a1c51b5aa58cd628d0832193742a
Alphatechnic <alphatechnicist@gmail.com>
2022-02-17 08:17:18 +0900

    [refactor] username, date를 전달해서 관련 GPS로그 받을 수 있는 테스트코드 작성

---
+++ dailypathapp/tests/test_print_GPSDayLog.py
@@ -0,0 +1,56 @@
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+from rest_framework.test import APITestCase
+from rest_framework import status
+from dailypathapp.models import GPSLog
+
+import os
+import datetime
+
+"""
+- shell 명령
+export TestDate=2020-02-06;
+export TestUser=F123;
+python manage.py test dailypathapp.tests.test_print_GPSDayLog;
+"""
+class MyDayLogClass(APITestCase):
+    username = ""
+    date = ""
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.username = os.environ["TestUser"]
+        cls.date = os.environ["TestDate"]
+        print("\n***** YOUR input DATA *****")
+        print(f"user: {cls.username}, date: {cls.date}")
+        print("***** YOUR input DATA *****\n")
+        pass
+
+    def test_print_GPSDayLog(self):
+        date = datetime.datetime.strptime(self.date, '%Y-%m-%d')
+        gpslogs = GPSLog.objects.filter(
+            daily_path__user__user__username__icontains=self.username,
+            timestamp__year=date.year,
+            timestamp__month=date.month,
+            timestamp__day=date.day,
+        )
+
+        data_to_print = dict()
+        if len(gpslogs) != 0:
+            gpslog_obj = gpslogs[0]
+            username = gpslog_obj.daily_path.user.user.username
+            data_to_print["user"] = username
+
+        data_to_print["timeSequence"] = list()
+        for gpslog in gpslogs:
+            timestamp = dict()
+            timestamp["time"] = gpslog.timestamp.strftime('%Y-%m-%d %H:%M:%S')
+            timestamp["coordinates"] = dict()
+            timestamp["coordinates"]["latitude"] = gpslog.latitude
+            timestamp["coordinates"]["longitude"] = gpslog.longitude
+            data_to_print["timeSequence"].append(timestamp)
+
+        print("***** THE output *****")
+        print(data_to_print)
+        print("***** THE output *****")

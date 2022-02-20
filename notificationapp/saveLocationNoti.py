42a95898af7db182849761a6fd2bb54b279003e4
Alphatechnic <alphatechnicist@gmail.com>
2022-02-20 18:16:46 +0900

    [refactor] saveLocation Test

--- notificationapp/saveLocationNoti.py
+++ notificationapp/saveLocationNoti.py
@@ -22,7 +22,7 @@ def start_save_location_noti():
 
     print(appuser_tokens)
     scheduler = BackgroundScheduler(timezone="Asia/Seoul", job_defaults={"max_instance": 1})
-    scheduler.add_job(func_to_schedule, 'cron', minute='15, 30',
+    scheduler.add_job(func_to_schedule, 'cron', minute='20, 30',
                       args=[appuser_tokens, False, "saveLocation", "saveLocation 통신", "saveLocation 통신"])
 
     scheduler.start()

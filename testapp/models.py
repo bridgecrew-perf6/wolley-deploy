53cd16c1a0eb47c5dfc0a2cd9bdcb629b1fccd57
mac <mac@macuiMaookPro65.localdomain>
2022-02-21 18:01:28 +0900

    [feat] Interval Stay 모델 update

--- testapp/models.py
+++ testapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class TestTable(models.Model):
-    # id = models.BigAutoField(primary_key=True)
+    id = models.BigAutoField(primary_key=True)
     textfield = models.TextField()  # 속성명 변경 text -> content(내용물)
 
     class Meta:

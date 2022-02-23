585591c8f024be5a09d018c22ed0231fa64aea4c
mac <mac@macui-MacBookPro-65.local>
2022-02-23 21:24:36 +0900

    [feat] badge 리스트 API

--- statisticapp/urls.py
+++ statisticapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import BadgeRequestView, DummyBadgeRequestView
+from .views import BadgeRequestView, DummyBadgeRequestView, BadgeListRequestView
 
 app_name = "statisticapp"
 
 urlpatterns = [
     path('badge/', BadgeRequestView.as_view(), name="badge_request"),
+    path('badge/list/', BadgeListRequestView.as_view(), name="badge_request"),
     path('badge/dummy/', DummyBadgeRequestView.as_view(), name="dummy_badge_request"),
 
 ]

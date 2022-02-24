f7cbf0621da4935030ff18ee07ee2dc5c109ba8b
mac <mac@macui-MacBookPro-65.local>
2022-02-24 21:09:25 +0900

    [fix] path/past 오류 수정

--- dailypathapp/views.py
+++ dailypathapp/views.py
@@ -282,7 +282,7 @@ class PathPastRequestView(APIView):
             return Response(content, status=status.HTTP_400_BAD_REQUEST)
 
         if not request_time_sequence:
-            content = make_response_content("time sequence 없음")
+            content = make_response_content("time sequence 없음", {})
             return Response(content, status=status.HTTP_400_BAD_REQUEST)
 
         daily_path_obj, created = DailyPath.objects.get_or_create(
